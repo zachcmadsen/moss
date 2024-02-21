ef9c7677d94fd40522533c112f7601496264a582
Zach Madsen <zach@zachmadsen.com>
2024-02-20 23:54:16 -0800

    Add a doctest test for ProcessorTests

--- cpu.cpp
+++ cpu.cpp
@@ -1,6 +1,12 @@
+#include <cstdint>
+#include <memory>
+
+#include "doctest.h"
+#include "simdjson.h"
+
 #include "cpu.h"
 
-#include <cstdint>
+using namespace simdjson;
 
 namespace Moss {
 void
@@ -13,4 +19,46 @@ Cpu::Step()
             break;
     }
 }
+}
+
+TEST_CASE("ProcessorTests")
+{
+    auto cpu = std::make_unique<Moss::Cpu>();
+
+    ondemand::parser parser;
+    auto json = padded_string::load("a9.json");
+
+    for (ondemand::object test : parser.iterate(json)) {
+        ondemand::object start = test["initial"];
+        cpu->Pc(static_cast<std::uint16_t>(start["pc"].get_int64()));
+        cpu->S(static_cast<std::uint8_t>(start["s"].get_int64()));
+        cpu->A(static_cast<std::uint8_t>(start["a"].get_int64()));
+        cpu->X(static_cast<std::uint8_t>(start["x"].get_int64()));
+        cpu->Y(static_cast<std::uint8_t>(start["y"].get_int64()));
+        cpu->P(static_cast<std::uint8_t>(start["p"].get_int64()));
+        for (ondemand::array entry : start["ram"].get_array()) {
+            auto it = entry.begin();
+            uint16_t addr = static_cast<std::uint16_t>((*it).get_int64());
+            ++it;
+            uint8_t data = static_cast<std::uint8_t>((*it).get_int64());
+            cpu->Write(addr, data);
+        }
+
+        cpu->Step();
+
+        ondemand::object end = test["final"];
+        CHECK(cpu->Pc() == end["pc"].get_int64());
+        CHECK(cpu->S() == end["s"].get_int64());
+        CHECK(cpu->A() == end["a"].get_int64());
+        CHECK(cpu->X() == end["x"].get_int64());
+        CHECK(cpu->Y() == end["y"].get_int64());
+        CHECK(cpu->P() == end["p"].get_int64());
+        for (ondemand::array entry : end["ram"].get_array()) {
+            auto it = entry.begin();
+            uint16_t addr = static_cast<std::uint16_t>((*it).get_int64());
+            ++it;
+            uint8_t data = static_cast<std::uint8_t>((*it).get_int64());
+            CHECK(cpu->Read(addr) == data);
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,53 +1,16 @@
-#include <cassert>
-#include <memory>
-
-#include <simdjson.h>
-
-#include "cpu.h"
-
-using namespace simdjson;
+// TODO: Define DOCTEST_CONFIG_DISABLE for release builds.
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
 
 int
-main()
+main(int argc, char* argv[])
 {
-    auto cpu = std::make_unique<Moss::Cpu>();
-
-    ondemand::parser parser;
-    auto json = padded_string::load("a9.json");
-
-    for (ondemand::object test : parser.iterate(json)) {
-        ondemand::object start = test["initial"];
-        cpu->Pc(static_cast<std::uint16_t>(start["pc"].get_int64()));
-        cpu->S(static_cast<std::uint8_t>(start["s"].get_int64()));
-        cpu->A(static_cast<std::uint8_t>(start["a"].get_int64()));
-        cpu->X(static_cast<std::uint8_t>(start["x"].get_int64()));
-        cpu->Y(static_cast<std::uint8_t>(start["y"].get_int64()));
-        cpu->P(static_cast<std::uint8_t>(start["p"].get_int64()));
-        for (ondemand::array entry : start["ram"].get_array()) {
-            auto it = entry.begin();
-            uint16_t addr = static_cast<std::uint16_t>((*it).get_int64());
-            ++it;
-            uint8_t data = static_cast<std::uint8_t>((*it).get_int64());
-            cpu->Write(addr, data);
-        }
-
-        cpu->Step();
-
-        ondemand::object end = test["final"];
-        assert(cpu->Pc() == end["pc"].get_int64());
-        assert(cpu->S() == end["s"].get_int64());
-        assert(cpu->A() == end["a"].get_int64());
-        assert(cpu->X() == end["x"].get_int64());
-        assert(cpu->Y() == end["y"].get_int64());
-        assert(cpu->P() == end["p"].get_int64());
-        for (ondemand::array entry : end["ram"].get_array()) {
-            auto it = entry.begin();
-            uint16_t addr = static_cast<std::uint16_t>((*it).get_int64());
-            ++it;
-            uint8_t data = static_cast<std::uint8_t>((*it).get_int64());
-            assert(cpu->Read(addr) == data);
-        }
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+    int doctestResult = context.run();
+    if (context.shouldExit()) {
+        return doctestResult;
     }
 
-    return 0;
+    return doctestResult;
 }
